4c318538dbb34f2964f0e91248095be4c67d0231
thomas@leese.io <thomas@leese.io>
2018-06-16 18:29:34 +0100

    Create initial food models

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Brand, Category, Food, Nutrition, Product, Serving, Tag
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = 'name', 'colour', 'website'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = 'name', 'colour'
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('singular_name',)}
+    list_display = 'singular_name', 'plural_name'
+
+
+@admin.register(Nutrition)
+class NutritionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = 'name', 'brand', 'gtin'
+
+
+@admin.register(Serving)
+class ServingAdmin(admin.ModelAdmin):
+    list_display = 'name', 'product', 'nutrition'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = 'name',
--- food/models.py
+++ food/models.py
@@ -1,3 +1,86 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=250, unique=True)
+    colour = models.CharField(max_length=25, default='black')
+    website = models.URLField(blank=True)
+
+    class Meta:
+        ordering = 'slug',
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=250, unique=True)
+    colour = models.CharField(max_length=25, default='black')
+
+    class Meta:
+        ordering = 'slug',
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+    slug = models.SlugField(unique=True)
+    singular_name = models.CharField(max_length=250, unique=True)
+    plural_name = models.CharField(max_length=250, unique=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='foods'
+    )
+    tags = models.ManyToManyField('Tag', related_name='foods')
+
+    class Meta:
+        ordering = 'slug',
+
+    def __str__(self):
+        return self.singular_name
+
+
+class Nutrition(models.Model):
+    pass
+
+
+class Product(models.Model):
+    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=250, unique=True)
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, related_name='products'
+    )
+    gtin = models.CharField(max_length=250, unique=True)
+
+    class Meta:
+        ordering = 'slug',
+
+    def __str__(self):
+        return self.name
+
+
+class Serving(models.Model):
+    name = models.CharField(max_length=250)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='servings'
+    )
+    nutrition = models.OneToOneField(
+        Nutrition, on_delete=models.CASCADE, related_name='serving'
+    )
+
+    def __str__(self):
+        return f'{self.name} of {self.product}'
+
+
+class Tag(models.Model):
+    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=250, unique=True)
+
+    class Meta:
+        ordering = 'slug',
+
+    def __str__(self):
+        return self.name
